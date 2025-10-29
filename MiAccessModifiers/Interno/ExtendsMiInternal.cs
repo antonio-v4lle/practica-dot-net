49b387f5bd2fbbe43380635d72334aba76065b69
default <antonio@v4lle.tech>
2025-10-29 17:20:30 +0000

    chore: modificadores de acesso en campos.

--- MiAccessModifiers/Interno/ExtendsMiInternal.cs
+++ MiAccessModifiers/Interno/ExtendsMiInternal.cs
@@ -5,6 +5,11 @@ namespace MiAccessModifiers.Interno
 
         public ExtendsMiInternal()
         {
+            // ArrowFunction(Getter) error CS0200: Property or indexer 'MiInternal.RealmenteProtegido' cannot be assigned to -- it is read only
+            // Con modificadores de acceso en getter/setter error CS0272: The property or indexer 'MiInternal.RealmenteProtegido' cannot be used in this context because the set accessor is inaccessible
+            // RealmenteProtegido = "lo que sea";
+            var valor = RealmenteProtegido;    // ✓ Solo puede leer
+            Console.WriteLine($"Constructor.ProtectedReadOnly: {valor}"); // ShouldBe: private-parent (string RealmenteProtegido => privateText;)
             sharedText += "-Extends";
             protectedText += "-Extends";
             internalText += "-Extends";
--- MiAccessModifiers/Interno/MiInternal.cs
+++ MiAccessModifiers/Interno/MiInternal.cs
@@ -2,9 +2,22 @@ namespace MiAccessModifiers.Interno
 {
     internal class MiInternal
     {
+        // Nivel de restricción mayor a menor
+        // private > protected > public
         public string sharedText { get; set; } = "parent";
-        protected string protectedText { get; set; } = "parent";
-        private string privateText { get; set; } = "parent";
+        protected string protectedText { get; set; } = "parent"; // Protegido solo hijo: pero con acceso a modificarlo
+        private string privateText { get; set; } = "private-parent"; // Solo padre
+        // ✓ Propiedad protected con arrow function - SOLO LECTURA
+        // protected string RealmenteProtegido => privateText; // Getter readonly
+        // Esto es lo mismo que:
+        protected string RealmenteProtegido
+        {
+            get => privateText;
+            // Setter tambien puede ser protected => Inmutabilidad: Evita modificaciones accidentales desde derivadas
+            // error CS0273: The accessibility modifier of the 'MiInternal.RealmenteProtegido.set' accessor must be more restrictive than the property or indexer 'MiInternal.RealmenteProtegido'
+            private set => privateText = value;
+        }
+
         internal string internalText { get; set; } = "parent";
         internal void DoSomethingInternal()
         {
