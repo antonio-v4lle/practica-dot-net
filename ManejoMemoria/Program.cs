8745537cf52dc63e5738a232b81d7cfbb93d5cae
default <antonio@v4lle.tech>
2025-11-05 15:43:47 +0000

    chore: benchmark alto rendimiento de memoria

---
+++ ManejoMemoria/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using BenchmarkDotNet.Running;
+
+
+// Program.cs
+class Program
+{
+    static void Main(string[] args)
+    {
+        // var summary = BenchmarkRunner.Run<StringConcatBenchmark>();
+        // var summary = BenchmarkRunner.Run<CollectionBenchmark>();
+        var summary = BenchmarkRunner.Run<PoolingBenchmark>();
+    }
+}
